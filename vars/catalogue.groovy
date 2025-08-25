e1ae2cde6885f4b46aaae04a50c391879e454fe5
Dustboy743 <dsaidns@gmail.com>
2025-08-26 02:59:22 +0530

    shared

--- vars/catalogue.groovy
+++ vars/catalogue.groovy
@@ -1,63 +1,66 @@
 def call(Map configMap){   //Map is datatype
-    environment {
-        appVersion = " "
-        REGION = "us-east-1"
-        ACC_ID = "127218179061"
-        PROJECT = configMap.get('project')
-        COMPONENT = configMap.get('component')
+    pipeline {
+        agent { label "agent-1"}   
+        environment {
+            appVersion = " "
+            REGION = "us-east-1"
+            ACC_ID = "127218179061"
+            PROJECT = configMap.get('project')
+            COMPONENT = configMap.get('component')
 
-    }
-    // Build
-    stages{
-        stage("build"){
-            steps{
-                script {
-                   def packageJson = readJSON file: 'package.json'     
-                   appVersion = packageJson.version
-                   echo "Package version: ${appVersion}"
-                }
-            }
-        stage("install dependencies")
-            steps{
-                script{
-                    sh """
-                    npm install
-                """
-                }
-            }    
         }
-        stage("Unit test"){
-            steps{
-                echo "unit test"
-            }
-        }
-        stage("Docker Build"){
-            steps{
-                script{
-                    withAWS(credentials: 'aws-cred', region: 'us-east-1') {
+        // Build
+        stages{
+            stage("build"){
+                steps{
+                    script {
+                    def packageJson = readJSON file: 'package.json'     
+                    appVersion = packageJson.version
+                    echo "Package version: ${appVersion}"
+                    }
+                }
+            stage("install dependencies")
+                steps{
+                    script{
                         sh """
-                                aws ecr get-login-password --region ${REGION} | docker login --username AWS --password-stdin ${ACC_ID}.dkr.ecr.us-east-1.amazonaws.com
-                                docker build -t ${ACC_ID}.dkr.ecr.us-east-1.amazonaws.com/${PROJECT}/${COMPONENT}:${appVersion} .
-                                docker push ${ACC_ID}.dkr.ecr.us-east-1.amazonaws.com/${PROJECT}/${COMPONENT}:${appVersion}
-                        """
-                }      
+                        npm install
+                    """
+                    }
+                }    
+            }
+            stage("Unit test"){
+                steps{
+                    echo "unit test"
                 }
+            }
+            stage("Docker Build"){
+                steps{
+                    script{
+                        withAWS(credentials: 'aws-cred', region: 'us-east-1') {
+                            sh """
+                                    aws ecr get-login-password --region ${REGION} | docker login --username AWS --password-stdin ${ACC_ID}.dkr.ecr.us-east-1.amazonaws.com
+                                    docker build -t ${ACC_ID}.dkr.ecr.us-east-1.amazonaws.com/${PROJECT}/${COMPONENT}:${appVersion} .
+                                    docker push ${ACC_ID}.dkr.ecr.us-east-1.amazonaws.com/${PROJECT}/${COMPONENT}:${appVersion}
+                            """
+                    }      
+                    }
 
+                }
+            }
+
+            post { 
+            always { 
+                echo 'I will always say Hello again!'
+                deleteDir()
+            }
+            success { 
+                echo 'Hello Success'
+            }
+            failure { 
+                echo 'Hello Failure'
             }
         }
 
-        post { 
-        always { 
-            echo 'I will always say Hello again!'
-            deleteDir()
         }
-        success { 
-            echo 'Hello Success'
-        }
-        failure { 
-            echo 'Hello Failure'
-        }
-    }
-
     }
 }
