9d733c2ccd71c068f92b4ed59df252d1598ba012
Dustboy743 <dsaidns@gmail.com>
2025-08-26 15:18:08 +0530

    shared

--- vars/catalogue.groovy
+++ vars/catalogue.groovy
@@ -1,54 +1,75 @@
 def call(Map configMap){   //Map is datatype
-    pipeline {
-        agent { label "agent-1"}   
-        environment {
-            appVersion = " "
-            REGION = "us-east-1"
-            ACC_ID = "127218179061"
-            PROJECT = configMap.get('project')
-            COMPONENT = configMap.get('component')
+   pipeline {
+    agent { label "agent-1"}
+    environment {
+        appVersion = " "
+        REGION = "us-east-1"
+        ACC_ID = "127218179061"
+        PROJECT = "roboshop"
+        COMPONENT = "catalogue"
 
-        }
-        // Build
-        stages{
-            stage("build"){
-                steps{
-                    script {
-                        def packageJson = readJSON file: 'package.json'     
-                        appVersion = packageJson.version
-                        echo "Package version: ${appVersion}"
-                    }
-                }
-            }    
-            stage("install dependencies") {
-                steps{
-                    script{
-                        sh """
-                        npm install
-                    """
-                    }
-                }    
-            }
-            stage("Unit test"){
-                steps{
-                    echo "unit test"
-                }
-            }
-            stage("Docker Build"){
-                steps{
-                    script{
-                        withAWS(credentials: 'aws-cred', region: 'us-east-1') {
-                            sh """
-                                    aws ecr get-login-password --region ${REGION} | docker login --username AWS --password-stdin ${ACC_ID}.dkr.ecr.us-east-1.amazonaws.com
-                                    docker build -t ${ACC_ID}.dkr.ecr.us-east-1.amazonaws.com/${PROJECT}/${COMPONENT}:${appVersion} .
-                                    docker push ${ACC_ID}.dkr.ecr.us-east-1.amazonaws.com/${PROJECT}/${COMPONENT}:${appVersion}
-                            """
-                    }      
-                    }
+    }
 
+    parameters {
+        booleanParam(name: 'deploy', defaultValue: false, description: 'Toggle this value')
+    }
+    // Build
+    stages {
+        stage('build') {
+            steps {
+                script {
+                   def packageJson = readJSON file: 'package.json'     
+                   appVersion = packageJson.version
+                   echo "Package version: ${appVersion}"
                 }
             }
         }    
+        stage('install dependencies') {
+            steps {
+                script{
+                    sh """
+                    npm install
+                """
+                }
+            }    
+        }
+        stage('Unit test'){
+            steps{
+                echo "unit test"
+            }
+        }
+        stage('Docker Build'){
+            steps{
+                script{
+                    withAWS(credentials: 'aws-cred', region: 'us-east-1') {
+                        sh """
+                                aws ecr get-login-password --region ${REGION} | docker login --username AWS --password-stdin ${ACC_ID}.dkr.ecr.us-east-1.amazonaws.com
+                                docker build -t ${ACC_ID}.dkr.ecr.us-east-1.amazonaws.com/${PROJECT}/${COMPONENT}:${appVersion} .
+                                docker push ${ACC_ID}.dkr.ecr.us-east-1.amazonaws.com/${PROJECT}/${COMPONENT}:${appVersion}
+                        """
+                }      
+                }
+
+            }
+        }
+
+        stage('Trigger Deploy') {
+            when{
+                expression { params.deploy }
+            }
+            steps {
+                script {
+                    build job: 'catalogue-cd',
+                    parameters: [
+                        string(name: 'appVersion', value: "${appVersion}"),
+                        string(name: 'deploy_to', value: 'dev')
+                    ],
+                    propagate: false,  // even SG fails VPC will not be effected
+                    wait: false // VPC will not wait for SG pipeline completion
+                }
+            }
+        }
+    }
 
         post { 
             always { 
@@ -61,7 +82,7 @@ def call(Map configMap){   //Map is datatype
             failure { 
                 echo 'Hello Failure'
             }
-        }
 
+        }
     }
 }
